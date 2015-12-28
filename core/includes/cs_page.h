1a272e81d8ed5c86b2035ab4eb07e689da852c0b
Nemo Yu <mzer0.yu@gmail.com>
2015-12-28 21:46:58 +0800

    Update cs_page.h

--- core/includes/cs_page.h
+++ core/includes/cs_page.h
@@ -1,7 +1,40 @@
-#include "pageProcesser.h"
+#ifndef CS_PAGE_H
+#define CS_PAGE_H
+#endif
+
+/*
+  page carrier
+*/
+
+#define FileTypeErr 0
+#define FileTypeHTML 1
+#define FileTypeCSS 2
+#define FileTypeJSON 3
+
+struct cs_page_struct {
+  void *data;
+  unsigned int capacity;
+  unsigned int used;
+  char file_type;
+};
+
+/*
+  page carrier'a queue
+*/
+
+#define MaxPageQueueNum 32
+#define LogMaxPageQueueNum 5
+
+struct cs_page_queue_struct {
+  cs_page *pages;
+  unsigned int capacity;
+  unsigned int usage;
+};
 
 void clear_page(cs_page *p);
-void destroy_page(cs_page *p);
-int new_page(cs_page *p, unsigned int capacity);
-int set_page(cs_page *p, char* context, unsigned int length);
 
+void destroy_page(cs_page *p);
+
+int new_page(cs_page *p, unsigned int capacity);
+
+int set_page(cs_page *p, char* context, unsigned int length);
